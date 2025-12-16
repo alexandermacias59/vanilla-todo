380a7cfbbcd1fd1d4fddf9111632c2ced9b63e4f
Alexander Macias <alexandermacias59@gmail.com>
2025-12-16 21:47:44 +0100

    esercizi svolti per il 17/12

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ function displayTodos(todos) {
     }
 }
 getAllTodos().then(results => displayTodos(results));
+
+
+function orderByAlfabetic(){
+    getAllTodos().then(results => {
+        results.sort((a, b) => a.title.localeCompare(b.title));
+        displayTodos(results);
+    }); 
+}
+const btnAlfabeticOrder = document.getElementById('btn-alfabetic-order');
+btnAlfabeticOrder.addEventListener('click', orderByAlfabetic);
+
+
+function orderByRecent(){
+    getAllTodos().then(results => {
+        results.sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
+        displayTodos(results);
+    })
+}
+const btnRecentOrder = document.getElementById('btn-recent-order');
+btnRecentOrder.addEventListener('click', orderByRecent);
